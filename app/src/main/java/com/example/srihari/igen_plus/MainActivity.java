629f5c905e263dbb2dfc0bb8ea3e417ab34ee4df
sriharibtl <hari.sri777@gmail.com>
2015-09-30 19:07:03 +0530

    init commit

---
+++ app/src/main/java/com/example/srihari/igen_plus/MainActivity.java
@@ -0,0 +1,35 @@
+
+package com.example.srihari.igen_plus;
+
+import android.content.Intent;
+import android.speech.RecognizerIntent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.SearchView;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    SearchView g1;
+    Button g2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        g2 = (Button) findViewById(R.id.g2);
+
+        g2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, MainActivity2Activity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
